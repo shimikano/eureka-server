2171eb3096ee4e56de27238f2c559ed01ca76868
shimikano <3526886+shimikano@users.noreply.github.com>
2020-08-20 11:36:45 +0200

    Code as generated by spring initializr.

---
+++ src/test/java/ch/yoshimi/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.yoshimi.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
